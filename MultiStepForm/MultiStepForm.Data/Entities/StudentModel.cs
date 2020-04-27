21db1cc9edbaeb86ab5d3480383845a512f363a1
TOUHID <muhdtohidul@gmail.com>
2020-04-28 03:21:51 +0600

    git commit implemented design pattern

---
+++ MultiStepForm/MultiStepForm.Data/Entities/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiStepForm.Data.Entities
+{
+    [Table("tb_Student")]
+    public class StudentModel
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string FathersName { get; set; }
+        public string MothersName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
---
+++ MultiStepForm/MultiStepForm.Data/ViewModels/StudentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiStepForm.Data.ViewModels
+{
+    public class StudentViewModel
+    {
+       
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string FathersName { get; set; }
+        public string MothersName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
